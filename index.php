065daae05f37f350205e8835e8ac66257a530a18
ducthangbui <ducthangbui1996@gmail.com>
2019-12-17 00:08:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
    error_log(implode("|",$_REQUEST));
    foreach (getallheaders() as $name => $value) {
     echo "$name: $value\n";
-      $curl = curl_init('https://api.telegram.org/bot937782324:AAFxgtrxcMuHlMMMK0JmT8H8-RnqTwsz7JY/sendMessage?chat_id=-261885730&text=test');
-      curl_exec($curl);
+    // Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://api.telegram.org/bot937782324:AAFxgtrxcMuHlMMMK0JmT8H8-RnqTwsz7JY/sendMessage?chat_id=-261885730&text=test'
+]);
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
 ?>
 
 <script>
